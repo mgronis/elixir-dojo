2514ebedbb14d529f6312bfab311903bc8ea36b1
mgronis <m.gronis@gmail.com>
2015-10-08 09:28:56 +0200

    FizzBuzz: Added tests up to input = 10

--- dojo/test/fizzbuzz_test.exs
+++ dojo/test/fizzbuzz_test.exs
@@ -21,4 +21,24 @@ defmodule FizzBuzzTest do
   test "input is 5 should return buzz" do
     assert go(5) == "buzz"
   end
+
+  test "input is 6 should return fizz" do
+    assert go(6) == "fizz"
+  end
+
+  test "input is 7 should return 7" do
+    assert go(7) == 7
+  end
+
+  test "input is 8 should return 8" do
+    assert go(8) == 8
+  end
+
+  test "input is 9 should return fizz" do
+    assert go(9) == "fizz"
+  end
+
+  test "input is 10 should return buzz" do
+    assert go(10) == "buzz"
+  end
 end
