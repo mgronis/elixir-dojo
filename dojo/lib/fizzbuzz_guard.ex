8dcab692e30c8accdc6640a7a2a58ef8088eaae5
mgronis <m.gronis@gmail.com>
2015-10-11 00:52:04 +0200

    Refactoring: Forgott to add the new files ><

---
+++ dojo/lib/fizzbuzz_guard.ex
@@ -0,0 +1,19 @@
+defmodule FizzBuzz do
+
+  def go(input) when rem(input, 3) == 0 and rem(input, 5) == 0 do
+    "fizzbuzz"
+  end
+
+  def go(input) when rem(input, 3)  == 0 do
+    "fizz"
+  end
+
+  def go(input) when rem(input, 5) == 0 do
+    "buzz"
+  end
+
+  def go(input) do
+    input
+  end
+
+end
