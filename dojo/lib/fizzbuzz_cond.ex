3985ed15ba38baf03e256a25dec82eea57036ce6
mgronis <m.gronis@gmail.com>
2015-10-12 08:38:47 +0200

    FizzBuzz: Added test and cond for 'fizz' and 'buzz'

--- dojo/lib/fizzbuzz_cond.ex
+++ dojo/lib/fizzbuzz_cond.ex
@@ -1,7 +1,14 @@
 defmodule FizzBuzzCond do
 
   def go(input) do
-    1
+    cond do
+      rem(input, 3) == 0 ->
+        "fizz"
+      rem(input, 5) == 0 ->
+        "buzz"
+      true ->
+        input
+    end
   end
 
 end
