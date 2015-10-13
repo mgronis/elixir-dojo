ca6beaf61f5052b060b8651bcd807d6840a50a88
mgronis <m.gronis@gmail.com>
2015-10-13 08:51:49 +0200

    FizzBuzz: Added test for fizz and buzz using case

--- dojo/lib/fizzbuzz_case.ex
+++ dojo/lib/fizzbuzz_case.ex
@@ -1,7 +1,18 @@
 defmodule FizzBuzzCase do
 
   def go(input) do
-    input
+    case divisibleBy(input, 3, 5) do
+      {0, _} ->
+        "fizz"
+      {_, 0} ->
+        "buzz"
+      _ ->
+        input
+    end
+  end
+
+  defp divisibleBy(input, first, second) do
+    { rem(input, first), rem(input, second) }
   end
 
 end
--- dojo/test/fizzbuzz_case_test.exs
+++ dojo/test/fizzbuzz_case_test.exs
@@ -2,8 +2,24 @@ defmodule FizzBuzzCaseTest do
   use ExUnit.Case
   import FizzBuzzCase
 
-  test "inpus is 1 should return 1" do
+  test "input is 1 should return 1" do
     assert go(1) == 1
+  end
+
+  test "input is 2 should return 2" do
+    assert go(2) == 2
+  end
+
+  test "input is 3 should return fizz" do
+    assert go(3) == "fizz"
+  end
+
+  test "input is 4 should return 4" do
+    assert go(4) == 4
+  end
+
+  test "input is 5 should return buzz" do
+    assert go(5) == "buzz"
   end
 
 end
