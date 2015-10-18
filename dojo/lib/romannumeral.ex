983473c9ccf75c954a6130699c50cc0b7c831d87
mgronis <m.gronis@gmail.com>
2015-10-18 22:31:49 +0200

    Romannumeral: At least any input gives I back is a start

---
+++ dojo/lib/romannumeral.ex
@@ -0,0 +1,7 @@
+defmodule Romannumeral do
+
+  def convert(input) do
+    "I"
+  end
+  
+end
--- dojo/test/romannumeral_test.exs
+++ dojo/test/romannumeral_test.exs
@@ -2,6 +2,8 @@ defmodule RomannumeralTest do
   use ExUnit.Case
   import Romannumeral
 
-  
+  test "input is 1 sould return I" do
+    assert convert(1) == "I" 
+  end
 
 end
