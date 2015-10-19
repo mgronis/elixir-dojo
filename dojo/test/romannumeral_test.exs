425e9b0cd9ebe0f3da655aa8d1e87b4c274a5648
mgronis <m.gronis@gmail.com>
2015-10-19 10:29:51 +0200

    Romannumeral: Got test that expects an exception to work

--- dojo/test/romannumeral_test.exs
+++ dojo/test/romannumeral_test.exs
@@ -2,9 +2,9 @@ defmodule RomannumeralTest do
   use ExUnit.Case
   import Romannumeral
 
-  # test "input is string should raise" do
-  #   assert_raise RuntimeError, doConvert("1")
-  # end
+  test "input is string should raise" do
+    assert_raise RuntimeError, fn -> doConvert("1") end
+  end
 
   test "input is 1 should return I" do
     assert doConvert(1) == "I"
