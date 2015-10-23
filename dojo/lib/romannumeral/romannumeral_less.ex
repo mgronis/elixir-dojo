c6e9705a2b0d02224035fd20a963c4b254bd1d67
mgronis <m.gronis@gmail.com>
2015-10-23 14:10:55 +0200

    Romannumeral: Commented code, starting small

--- dojo/lib/romannumeral/romannumeral_less.ex
+++ dojo/lib/romannumeral/romannumeral_less.ex
@@ -8,25 +8,25 @@ defmodule RomannumeralLess do
 
   defp convert(0), do: ""
   defp convert(4), do: "IV"
-  defp convert(9), do: "IX"
-  defp convert(40), do: "XL"
-  defp convert(90), do: "XC"
-
-  defp convert(input) when input >= 91 do
-    "XC" <> convert(input - 90)
-  end
-
-  defp convert(input) when input >= 50 do
-    "L" <> convert(input - 50)
-  end
-
-  defp convert(input) when input >= 41 do
-    "XL" <> convert(input - 40)
-  end
-
-  defp convert(input) when input >= 10 do
-    "X" <> convert(input - 10)
-  end
+  # defp convert(9), do: "IX"
+  # defp convert(40), do: "XL"
+  # defp convert(90), do: "XC"
+  #
+  # defp convert(input) when input >= 91 do
+  #   "XC" <> convert(input - 90)
+  # end
+  #
+  # defp convert(input) when input >= 50 do
+  #   "L" <> convert(input - 50)
+  # end
+  #
+  # defp convert(input) when input >= 41 do
+  #   "XL" <> convert(input - 40)
+  # end
+  #
+  # defp convert(input) when input >= 10 do
+  #   "X" <> convert(input - 10)
+  # end
 
   defp convert(input) when input >= 5 do
     "V" <> convert(input - 5)
