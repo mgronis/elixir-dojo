5c27d03259ac0998f54e00d3725a81a2f1319e19
mgronis <m.gronis@gmail.com>
2015-10-25 11:57:39 +0100

    Romannumeral: Removed commented code that is no longer needed

--- dojo/lib/romannumeral/romannumeral_less.ex
+++ dojo/lib/romannumeral/romannumeral_less.ex
@@ -9,26 +9,6 @@ defmodule RomannumeralLess do
   def doConvert(input), do: raise "input is not a number"
 
   defp convert(0, _, acc), do: acc
-  # defp convert(4, _, acc), do: "IV"
-  # defp convert(9), do: "IX"
-  # defp convert(40), do: "XL"
-  # defp convert(90), do: "XC"
-  #
-  # defp convert(input) when input >= 91 do
-  #   "XC" <> convert(input - 90)
-  # end
-  #
-  # defp convert(input) when input >= 50 do
-  #   "L" <> convert(input - 50)
-  # end
-  #
-  # defp convert(input) when input >= 41 do
-  #   "XL" <> convert(input - 40)
-  # end
-  #
-  # defp convert(input) when input >= 10 do
-  #   "X" <> convert(input - 10)
-  # end
 
   defp convert(input, prefixes = [[prefix, roman]|tail], acc) when input >= prefix do
     convert(input - prefix, prefixes, acc <> roman)
