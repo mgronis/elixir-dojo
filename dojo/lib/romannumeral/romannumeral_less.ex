880cff956bf25cf5855d327cbd50634cd2faa5b4
mgronis <m.gronis@gmail.com>
2015-10-25 11:59:56 +0100

    Romannumeral: Refactoring, went back to the idea about using list of tuples instad of list of lists

--- dojo/lib/romannumeral/romannumeral_less.ex
+++ dojo/lib/romannumeral/romannumeral_less.ex
@@ -1,6 +1,6 @@
 defmodule RomannumeralLess do
 
-  @prefixes [[5, "V"], [4, "IV"], [1, "I"]]
+  @prefixes [{5, "V"}, {4, "IV"}, {1, "I"}]
 
   def doConvert(input) when is_number input do
     convert(input, @prefixes, "")
@@ -10,11 +10,11 @@ defmodule RomannumeralLess do
 
   defp convert(0, _, acc), do: acc
 
-  defp convert(input, prefixes = [[prefix, roman]|tail], acc) when input >= prefix do
+  defp convert(input, prefixes = [{prefix, roman}|tail], acc) when input >= prefix do
     convert(input - prefix, prefixes, acc <> roman)
   end
 
-  defp convert(input, [[_,_]|tail], acc) do
+  defp convert(input, [{_,_}|tail], acc) do
     convert(input, tail, acc)
   end
 
