baadd498e81989463b62c7b184702c22eacc6577
mgronis <m.gronis@gmail.com>
2015-10-29 10:47:44 +0100

    Bowling: First try at implementing the easiest case 2 frames of 2 rolls each

--- dojo/lib/bowling/bowling.ex
+++ dojo/lib/bowling/bowling.ex
@@ -1,3 +1,6 @@
 defmodule Bowling do
 
+  def calculate(frames) do
+    Enum.reduce(frames, 0, fn({a, b}, acc) -> (a + b + acc) end)
+  end
 end
--- dojo/test/bowling/bowling.exs
+++ dojo/test/bowling/bowling.exs
@@ -2,4 +2,8 @@ defmodule BowlingTest do
 use ExUnit.Case
 import Bowling
 
+  test "calculate two frames no strike/spare" do
+     calculate [{1,1}, {2,4}] == 8
+  end
+
 end
