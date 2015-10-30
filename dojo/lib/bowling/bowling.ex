7db1686069602187d82969537587bb2149e2bab0
mgronis <m.gronis@gmail.com>
2015-10-30 18:14:03 +0100

    Bowling: started workin on how to handle the last frame

--- dojo/lib/bowling/bowling.ex
+++ dojo/lib/bowling/bowling.ex
@@ -1,7 +1,16 @@
 defmodule Bowling do
 
   def calculate(frames) do
-    Enum.reduce(frames, 0, fn({a, b}, acc) -> (a + b + acc) end)
+    # Enum.reduce(frames, 0, fn({a, b}, acc) -> (a + b + acc) end)
+    Enum.reduce(frames, 0, &addScore(&1, &2))
+  end
+
+  defp addScore(frame, acc) do
+    case frame do
+      {a, b} -> a + b + acc
+      {a, b, c} -> a + b + c + acc
+      _ -> raise "Holy moly!! Your are cheating, that is not a bowling score...."
+    end
   end
 
 end
