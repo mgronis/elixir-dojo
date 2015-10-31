6ad77da588e8dcf966055d7b81309107bcfff65f
mgronis <m.gronis@gmail.com>
2015-10-31 09:47:38 +0100

    Bowling: Added simplyfied test for last frame calculation

--- dojo/test/bowling/bowling_test.exs
+++ dojo/test/bowling/bowling_test.exs
@@ -6,8 +6,8 @@ defmodule BowlingTest do
      assert calculate([{1,1}, {2,4}]) == 8
   end
 
-  # test "calculate two frames of which last is three rolls no strike/spare" do
-  #    assert calculate([{1,1}, {2,4,3}]) == 11
-  # end
+  test "calculate two frames of which last is three rolls no strike/spare" do
+     assert calculate([{1,1}, {2,4,3}]) == 11
+  end
 
 end
