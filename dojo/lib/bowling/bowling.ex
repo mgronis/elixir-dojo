ac4c10fe570e797370e13f2899ca9854ec714da6
mgronis <m.gronis@gmail.com>
2015-11-12 14:05:10 +0100

    Bowling: Refactoring, removed the last case and replaced it with pattern matching

--- dojo/lib/bowling/bowling.ex
+++ dojo/lib/bowling/bowling.ex
@@ -6,28 +6,24 @@ defmodule Bowling do
   end
 
   defp addScore({'x', '-'}=frame, {peek1, peek2, acc}) do; {frame, peek1, strikeScore(peek1, peek2, acc)} end
-  defp addScore({a, '/'}=frame, {peek1, peek2, acc}) do; {frame, peek1, spareScore(peek1, acc)} end
-  defp addScore({'-', '-'}, {peek1, peek2, acc}) do; {{0, 0}, peek1, acc} end
-  defp addScore({a, '-'}, {peek1, peek2, acc}) do; {{a, 0}, peek1, a + acc} end
-  defp addScore({'-', b}, {peek1, peek2, acc}) do; {{0, b}, peek1, b + acc} end
-  defp addScore({a, b}=frame, {peek1, peek2, acc}) do; {frame, peek1, a + b + acc} end
+  defp addScore({_a, '/'}=frame, {peek1, _, acc}) do; {frame, peek1, spareScore(peek1, acc)} end
+  defp addScore({'-', '-'}, {peek1, _, acc}) do; {{0, 0}, peek1, acc} end
+  defp addScore({a, '-'}, {peek1, _, acc}) do; {{a, 0}, peek1, a + acc} end
+  defp addScore({'-', b}, {peek1, _, acc}) do; {{0, b}, peek1, b + acc} end
+  defp addScore({a, b}=frame, {peek1, _, acc}) do; {frame, peek1, a + b + acc} end
 
-  defp addScore({'x', 'x', 'x'}, {peek1, peek2, acc}) do; {{'x', '-'}, {'x', '-'}, 30 + acc} end
-  defp addScore({_, _, _}=frame, {peek1, peek2, acc}) do
-    case frame do
-      {'x', 'x', '-'} -> {{'x', '-'}, {'x', '-'}, 20 + acc}
-      {'x', 'x', c} -> {{'x', '-'}, {'x', '-'}, 20 + c + acc}
-      {'x', b, '/'} -> {{'x', '-'}, {b, '/'}, 20 + acc}
-      {a, '/', 'x'} -> {{a, '/'}, {'x', '-'}, 20 + acc}
-      {a, '/', '-'} -> {{a, '/'}, {'-', '-'}, 10 + acc}
-      {a, '/', c} -> {{a, '/'}, {c, '-'}, 10 + c + acc}
-      {a, b, '-'} -> {{a, b}, peek1, a + b + acc}
-    end
-  end
+  defp addScore({'x', 'x', 'x'}, {_, _, acc}) do; {{'x', '-'}, {'x', '-'}, 30 + acc} end
+  defp addScore({'x', 'x', '-'}, {_, _, acc}) do; {{'x', '-'}, {'x', '-'}, 20 + acc} end
+  defp addScore({'x', 'x', c}, {_, _, acc}) do; {{'x', '-'}, {'x', '-'}, 20 + c + acc} end
+  defp addScore({'x', b, '/'}, {_, _, acc}) do; {{'x', '-'}, {b, '/'}, 20 + acc} end
+  defp addScore({a, '/', 'x'}, {_, _, acc}) do; {{a, '/'}, {'x', '-'}, 20 + acc} end
+  defp addScore({a, '/', '-'}, {_, _, acc}) do; {{a, '/'}, {'-', '-'}, 10 + acc} end
+  defp addScore({a, '/', c}, {_, _, acc}) do; {{a, '/'}, {c, '-'}, 10 + c + acc} end
+  defp addScore({a, b, '-'}, {peek1, _, acc}) do; {{a, b}, peek1, a + b + acc} end
 
   defp strikeScore({'x', '-'}, {'x', '-'}, acc) do; 30 + acc end
   defp strikeScore({'x', '-'}, {a, _}, acc) do; 20 + a + acc end
-  defp strikeScore({a, '/'}, _, acc) do; 20 + acc end
+  defp strikeScore({_a, '/'}, _, acc) do; 20 + acc end
   defp strikeScore({a, b}, _, acc) do; 10 + a + b + acc end
 
   defp spareScore({'x', _}, acc) do; 20 + acc end
